3efcdab4f369dcacd6a98130a4f2a5cbe01a9b47
tijjani-yusuf <tijjani.yusuf@dreamlabs.com.ng>
2022-11-06 23:45:50 +0100

    task 2

--- index.php
+++ index.php
@@ -1,12 +1,71 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Content-type: application/json');
+
+
+if(isset($_POST)){
+
+    // print_r($_POST);
+
+    if(isset($_POST['operation_type']) && !empty($_POST['operation_type']) &&isset($_POST['x']) && !empty($_POST['x']) && isset($_POST['y']) && !empty($_POST['y']) ){
+
+        $operation = $_POST['operation_type'];
+        $x = $_POST['x'];
+        $y = $_POST['y'];
+
+
+        try {
+            switch ($operation) {
+                case 'addition':
+                    $operation_type = 'addition';
+                    $result = $x+$y;
+                    break;
+                case 'subtraction':
+                    $operation_type = 'subtraction';
+                    $result = $x-$y;
+                    break;
+                case 'multiplication':
+                    $operation_type = 'multiplication';
+                    $result = $x*$y;
+                    break;
+                
+                default:
+                    echo errorResponse(); exit();
+                    break;
+            }     
+
+            $response = array(
+                'slackUsername' => 'Tj',
+                'result' => $result,
+                'operation_type' => $operation_type
+            );
+
+            http_response_code(200);
+
+            echo json_encode($response);             
+
+
+        } catch (Exception $e) {
+            echo errorResponse();        
+        }
+
+    }else {
+        echo errorResponse();
+    }
+
+}
+
+
+function errorResponse()
+{
     $response = array(
-        'slackUsername' => 'Tj',
-        'backend' => true,
-        'age' => 25, 
-        'bio' => 'I am graduate of Information Technology and a fullstack software engineer.'
+        'status' => 'error',
+        'message' => 'An error occured'
     );
 
+    http_response_code(500);
 
-echo json_encode($response); 
+    return json_encode($response);
+}
+
+
